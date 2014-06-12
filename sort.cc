f2c0bec26dd099cf916f1b5e87af3dd87609fd39
Yue Xing <yxing@coverity.com>
2014-06-12 15:19:42 -0700

    Implement sort.

--- sort.cc
+++ sort.cc
@@ -59,6 +59,7 @@ void merge_sub(int *arr, int left, int right)
     delete[] tmp;
 }
 
+// 1) divide into two halves, sort and merge
 void merge_sort(int *arr, int size)
 {
     merge_sub(arr, 0, size);
@@ -96,13 +97,118 @@ void quick_sub(int *arr, int left, int right)
     quick_sub(arr, pidx+1, right);
 }
 
+// each loop get a pivot to split the array into two
+// quick sort on each part
 void quick_sort(int *arr, int size)
 {
     quick_sub(arr, 0, size);
 }
 
-// how?
-void radix_sort(int *arr, int size)
+void filter_min_down(int *arr, int size)
 {
+    int i = 0;
+    while(i < size) {
+        int left = i * 2 + 1, right = i * 2 + 2, max = -1;
+        if(left < size && right < size) {
+            max = (arr[left] > arr[right]) ? left : right;
+        } else if(left < size) {
+            max = left;
+        } else return;
 
+        if(arr[max] > arr[i]) {
+            int tmp = arr[max];
+            arr[max] = arr[i];
+            arr[i] = tmp;
+        } else {
+            return;
+        }
+        i = max;
+    }
+}
+
+void de_max(int *arr, int size)
+{
+    int tmp = arr[0];
+    arr[0] = arr[size - 1];
+    arr[size - 1] = tmp;
+
+    filter_min_down(arr, size - 1);
+}
+
+void filter_max_up(int *arr, int i)
+{
+    while(i > 0) {
+        int c = i/2;
+        if(arr[c] < arr[i]) {
+            int tmp = arr[i];
+            arr[i] = arr[c];
+            arr[c] = tmp;
+        } else {
+            return;
+        }
+        i = c;
+    }
+}
+
+void build_max_heap(int *arr, int size)
+{
+    for(int i = 1; i < size; ++i) {
+        filter_max_up(arr, i);
+    }
+}
+
+// 1) build the max heap
+// 2) pop the front and put it at the end
+void heap_sort(int *arr, int size)
+{
+    build_max_heap(arr, size);
+    for(int i = 0; i < size; ++i) {
+        de_max(arr, size - i);
+    }
+}
+
+int max(int *arr, int size)
+{
+    int max = 0;
+    for(int i = 1; i < size; ++i) {
+        if(arr[i] > arr[max]) {
+            max = i;
+        }
+    }
+    return arr[max];
+}
+
+int extract(int n, int e)
+{
+    return (n / (e/10)) % 10;
+}
+
+void count_sort(int *arr, int size, int e, int range)
+{
+    int counts = new int[range];
+    int outs = new int[size];
+    for(int i = 0; i < size; ++i) {
+        counts[extract(arr[i], e)]++;
+        outs[i] = arr[i];
+    }
+    // the accumulated number 
+    for(int i = 1; i < range; ++i) {
+        counts[i] += counts[i-1];
+    }
+    for(int i = size-1; i > 0; --i) {
+        arr[counts[extract(arr[i], e)] - 1] = arr[i];
+        --counts[extract(arr[i], e)];
+    }
+    delete[] outs;
+    delete[] counts;
+}
+
+// counting sort
+// the complexity: log_range(max) * (size + range)
+void radix_sort(int *arr, int size, int range)
+{
+    int max = max(arr, size); 
+    for(e = 10; m/10; e *= 10) {
+        count_sort(arr, size, e, range);
+    }
 }
