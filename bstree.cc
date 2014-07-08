f13d2d5a29f9051571829e380c9aecd60c7f3b0e
Yue Xing <yxing@coverity.com>
2014-07-08 16:29:14 -0700

    Add some code to binary-search-tree.

---
+++ bstree.cc
@@ -0,0 +1,71 @@
+/**
+ * Functions manipulating binary trees.
+ * 
+ */
+
+// write a iterator which traverse the tree in a post-order such that the
+// consumer can just call operator++ to retrieve the next one.
+
+struct Node
+{
+    Node* left, right;
+};
+
+extern void visit(/*nullable*/Node*);
+
+// pre-order
+void pre_order(Node *root)
+{
+    stack<Node*> s;
+
+    while(root || !s.empty()) {
+        if(!root) {
+            root = s.pop();
+        }
+        visit(root);
+        root = root->left;
+        if(root->right) {
+            s.push(root->right);
+        }
+    }
+}
+
+// in-order
+void in_order(Node *root)
+{
+    stack<Node*> s;
+
+    while(1)
+    {
+        while(root) {
+            visit(root->left);
+            s.push(root);
+            root = root->left;
+        }
+        while(!root && !s.empty()) {
+            root = s.pop();
+            visit(root);
+            root = root->right;
+        }
+        if(!root && s.empty()) break;
+    }
+}
+
+// post-order
+// I think 2 stacks can help.
+void post_order(Node *root)
+{
+
+}
+
+// bounded AVL tree
+// basic (symmetric) cases:
+//    *   *
+//  *   *
+//*       *
+
+// red-black tree
+// 1. red cannot have red children
+// 2. the # of black has to be the same on every path
+// 3. the root of the tree is black.
+// Also, red-black tree is said to be combination of bst and btree.
