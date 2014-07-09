7febe80e804a7959a6be6703b5d9d150aef25fd6
Yue Xing <yxing@coverity.com>
2014-07-09 13:48:25 -0700

    Fix a resource-leak of stringb.

--- words/stringb.hpp
+++ words/stringb.hpp
@@ -5,7 +5,23 @@
 #include <sstream>
 using namespace std;
 
-#define stringb(stuff) \
-    ((static_cast<ostringstream*>(&(*(new ostringstream()) << stuff)))->str())
+struct oss_wrapper
+{
+public:
+    oss_wrapper() :
+        oss(new ostringstream()) {
+        }
+    ~oss_wrapper() {
+        delete oss;
+    }
+    ostream &get_stream() 
+    {
+        return *oss;
+    }
+private:
+    ostringstream *oss;
+};
 
+#define stringb(stuff)          \
+    static_cast<ostringstream*>(&(oss_wrapper().get_stream() << stuff))->str()
 #endif /* STRINGB_HPP */
