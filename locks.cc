a40f2d617d975d50f7bf8b836e180910649f87dc
Yue Xing <amyue.xing@gmail.com>
2014-07-21 23:03:09 -0700

    Add some thoughts and code about lock-free.

--- locks.cc
+++ locks.cc
@@ -1,9 +1,16 @@
-extern "C" int xchg(int);
+#include <queue>
+using namespace std;
+
+extern int getpid();
+extern void deschedule(bool);
+extern void make_runnable(int);
+
+extern int xchg(int*, int);
 // spin-lock
 struct spin_lock_t
 {
     void lock() {
-        while(!xchg(&avail, 0)) /*yield to the owner.*/;
+        while(xchg(&avail, 0) == 0) /*yield to the owner.*/;
     }
 
     // locked, so no need for the xchg
@@ -11,7 +18,7 @@ struct spin_lock_t
         avail = 1;
     }
 
-    spin_lock_t(): lock(1) {
+    spin_lock_t(): avail(1) {
     }
 private:
     int avail;
@@ -33,7 +40,7 @@ struct lock_t
         alock.lock();
         if(!avail) {
             wait_node_t n(getpid());
-            q.push_back(n);
+            q.push(n);
             alock.unlock(); 
 
             deschedule(n.to_wait);
@@ -49,11 +56,12 @@ struct lock_t
             avail = 1;
             alock.unlock();
         } else {
-            wait_node_t n = q.pop();
+            wait_node_t n = q.front();
+            q.pop();
             alock.unlock();
 
             n.to_wait = false;
-            schedule(n.pid); 
+            make_runnable(n.pid); 
         }
     }
 
@@ -61,7 +69,7 @@ private:
     spin_lock_t alock;
 
     int avail; 
-    queue<int> q; 
+    queue<wait_node_t> q; 
 };
 
 // condvar
@@ -71,7 +79,7 @@ struct condvar_t
     {
         qlock.lock();
         wait_node_t n(getpid());
-        q.push_back(n);
+        q.push(n);
         lock.unlock(); // suppose the seq does not matter now.
         qlock.unlock(); 
 
@@ -83,11 +91,12 @@ struct condvar_t
     {
         qlock.lock();
         if(!q.empty()) {
-            wait_node_t n = q.pop();
+            wait_node_t n = q.front();
+            q.pop();
             qlock.unlock();
 
             n.to_wait = false;
-            schedule(n.pid); 
+            make_runnable(n.pid); 
         } else {
             qlock.unlock();
         }
@@ -97,15 +106,16 @@ struct condvar_t
     {
         qlock.lock();
         while(!q.empty()) {
-            wait_node_t n = q.pop();
+            wait_node_t n = q.front();
+            q.pop();
             n.to_wait = false;
-            schedule(n.pid);
+            make_runnable(n.pid);
         }
         qlock.unlock();
     }
 
 private:
-    queue<int> q;
+    queue<wait_node_t> q;
     lock_t qlock;
 };
 
@@ -120,7 +130,7 @@ struct sem_t
         while(resources <= 0) {
             rcond.wait(rlock);
         }
-        --resource;
+        --resources;
         rlock.unlock();
     }
 
@@ -153,9 +163,9 @@ namespace producer_customer {
     struct consumer {
         void run() {
             full.wait();
-            empty.singal();
+            empty.signal();
         }
-    }
+    };
 };
 
 // read-write lock
@@ -225,6 +235,82 @@ struct rw_lock_t
     }
 
 private:
+    lock_t cnt_lock;
+    condvar_t rcond, wcond;
+    int waiting_writers, waiting_readers, readers, writer_pid;
 };
 
 // TODO: manipulate the list lock-free in multi-thread env.
+extern void* cxchg(void *from, void *to, void* expect);
+namespace {
+struct list_node_t
+{
+    int v;
+    list_node_t *next;
+
+    list_node_t(int v): v(v), next(NULL) {}
+};
+
+struct list_t
+{
+    list_t(): head(-1), tail(&head) {}
+
+    void push(int v)
+    {
+        list_node_t *n = new list_node_t(v);
+        while(cxchg(tail->next, NULL, n) == NULL);
+        tail = n; // this is atomic 
+    }
+
+    void insert_after(list_node_t *t, list_node_t* v)
+    {
+        do {
+            v->next = t->next; 
+        } while(cxchg(v, t->next, v->next) == v->next);
+    }
+
+    void delete_after(list_node_t *t)
+    {
+        list_node_t* v;
+        do {
+            v = t->next;
+        } while(cxchg(t->next, v, v->next) == v);
+        // NB: at this state, both v and t are pointing to the same v->next,
+        // which is very unsafe!
+        // So, the difference btw insert and delete is that the node to be deleted 
+        // has already been shared.
+        delete v;
+    }
+
+    // This version of push_front/pop_front can result to the ABA problem.
+    // global = new...;
+    //                            local2 = global;
+    // local1 = global;          
+    // free(local1);         
+    // global = new...;          
+    //                           if(local2 == global)
+    // btw the 2 reads for the second thread, the global has already changed!
+    // When the global is the head, and the test is cxchg(head, local2, local2->next),
+    // the list will result in pointing to garbage.
+    void pop()
+    {
+       list_node_t *n;
+       do {
+           n = head.next;
+       } while(cxchg(head.next, n, n->next));
+    }
+
+private:
+    list_node_t head, *tail;
+};
+
+// RCU:
+// rcu_read_lock, rcu_read_unlock: non-preemptive
+// rcu_fetch, rcu_assign
+// rcu_synchronize: wait for all context-switch have finished.
+}
+
+// safe delete a folder in multi-thread env
+// lock on the edge? 
+// lock-handoff: lock(prev) -> lock(root), lock(next), unlock(prev). But the chilren of the prev suffers multi-thread!
+// read-write lock on each node? Yes!
