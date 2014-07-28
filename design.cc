1dd586a14ddf6ee52e4c1576d00c9675483be038
Yue Xing <yxing@coverity.com>
2014-07-28 13:20:24 -0700

    Add design problems.

---
+++ design.cc
@@ -0,0 +1,10 @@
+// Design a Message store system  (in-memory storage) [seq_id, len, data] 
+// chunk
+
+// Design monitoring system, circular array, storage, aggregation
+
+// Design tinyURL
+
+// Design a key / value system
+
+// Design a feeds system, write and query
