d7723900e3a2097e7538605561082102f2cb98e8
Yue Xing <amyue.xing@gmail.com>
2014-08-06 08:44:04 -0700

    Add the rng implementation.

--- random.cc
+++ random.cc
@@ -37,3 +37,50 @@ int rng()
 // given the following constraints, generate numbers:
 // 1) sum to x;
 // 2) each is in the range of [min, max)
+
+// x ^ (x << 1)
+// the number will loop
+
+// the C version of xorshift
+uint32_t x, y, z, w;
+ 
+uint32_t xor128(void) {
+    uint32_t t;
+ 
+    t = x ^ (x << 11);
+    x = y; y = z; z = w;
+    return w = w ^ (w >> 19) ^ t ^ (t >> 8);
+}
+
+// The example implementation of libc
+// linear congruential generator
+int myrand(void) {
+    static unsigned long next = 1;
+    next = next * 1103515245 + 12345;
+    return((unsigned)(next/65536) % 32768);
+}
+
+#include <iostream>
+#include <fstream>
+// read from /dev/random, /dev/urandom
+struct rand_t
+{
+    rand_t(): ifs("/dev/random", ifstream::in)
+    {
+    }
+    int operator()()
+    {
+        uint32_t v;
+        ifs.read((char*)&v, 4);
+        return v % 32768;
+    }
+private:
+    ifstream ifs;
+};
+
+// run a hash function against a frame of a video stream from an unpredictable source
+
+// frequency test
+// serial test: 
+// poker test: 
+// gap test
