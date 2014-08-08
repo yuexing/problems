bf0ea68556654d18101e885cb514edc3c1ace968
Yue Xing <yxing@coverity.com>
2014-08-08 10:52:33 -0700

    Add more to bash template.

--- bash-template.sh
+++ bash-template.sh
@@ -27,8 +27,40 @@ if [[ $i > 1 ]]; then
     echo "definitely should use [[]]";
 fi
 
+if [[ "abc" = a* ]]; then
+    echo "wildcard glob works";             # yeah!
+fi
+
+if [[ 10 > 8 ]]; then
+    echo "arithmetic comparison"
+else
+    echo "Ah... string comparison"          # sucks!
+fi
+
+if [[ 10 -gt 8 ]]; then
+    echo "arithmetic comparison"            #yeah!
+fi
+
 LIST=$(ls);
 echo $LIST;
+
+# A safe way for executing commands
+options=( "-a" "-d" "-l" )
+options+=("foo")
+options+=("bar")                            #array append
+# If the index number is @ or *, all members of an array are referenced
+# array size
+echo ${#options[@]};
+# element size
+echo ${#options[1]};
+# slice
+echo ${options[@]:3:2};
+# replace
+echo ${options[@]/foo/fooo};
+# loop
+for i in ${options[@]}; do
+    echo $i;
+done
 
 # use egrep for extended-regex-expression, including ?/+/(/etc.
 # use fgrep for literally match
