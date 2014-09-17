3fb8576ebbfe13174f374d79ec5f615e5a6b59ca
Yue Xing <yxing@coverity.com>
2014-09-16 17:48:44 -0700

    Add a sm framework

---
+++ sm.cc
@@ -0,0 +1,90 @@
+struct state_t;
+
+struct node_t
+{
+    node_t *const next;
+};
+
+struct cache_t
+{
+    bool check_cache_hit(const state_t* s, bool first_time) ;
+};
+
+struct composed_ncache_t : public cache_t
+{
+    void add(cache_t *);
+    cache_t *nth(int n);
+
+    bool check_cache_hit(const state_t* s);    
+};
+
+struct state_t
+{
+    cache_t *create_empty_cache();    
+
+    // always widen in caching
+    bool widens();
+    void widen(cache_t *);
+};
+
+#if 0
+bool cache_t::check_cache_hit(const state_t* s, bool first_time) 
+{
+    // widen
+    if(s->widens()) {
+        s->widen(this);
+    }
+    // check hash hit based on size_t
+}
+#endif
+
+struct composed_nstate_t : public state_t
+{
+    void add(state_t *);
+    state_t *nth(int n);
+
+    cache_t *create_empty_cache();    
+
+    bool widens();
+    void widen(cache_t *);
+};
+
+class sm_t
+{
+    state_t *create_empty_state();
+    void handle(const node_t *n, state_t *s);
+};
+
+struct composed_nsm_t : public sm_t
+{
+    void add(sm_t *);
+    sm_t *nth(int n);
+    int idx(sm_t *);
+
+    state_t *create_empty_state();
+    void handle(const node_t *n, state_t *s);
+};
+
+class abstract_interp
+{
+    void add(sm_t *sm);
+    state_t *get_state(sm_t *sm);
+
+    void run(const node_t *n) {
+        bool first_time = true;
+        state_t *state = nsm.create_empty_state();
+        cache_t *cache = state->create_empty_cache();
+        while(n) {
+           nsm.handle(n, state); 
+           n = n->next;
+           if(first_time) {
+               first_time = false;
+           }
+           if(cache->check_cache_hit(state, first_time)) {
+               break;
+           }
+        }
+    }
+private:
+    composed_nsm_t nsm;
+};
